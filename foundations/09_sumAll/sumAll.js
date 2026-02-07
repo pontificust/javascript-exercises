d501f8a074f83174f409dfeb09ee9520f1e15e69
Pontificust <lihtinmann88@yandex.ru>
2026-02-07 12:18:03 +0300

    Solve the 09_sumAll task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0) {
+        const startNumber = Math.min(a, b);
+        let resultSum = 0;
 
+        for(let i = startNumber; i <= a + b - startNumber; i += 1) {
+            resultSum += i;
+        }
+        return resultSum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line

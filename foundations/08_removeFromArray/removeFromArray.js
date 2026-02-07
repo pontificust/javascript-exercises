2d8b59d2e24559f19906bddaefd69c130463c043
Pontificust <lihtinmann88@yandex.ru>
2026-02-07 11:57:33 +0300

    Solve the 08_removeFromArray task

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    for(let i = 0; i < valuesToRemove.length; i += 1) {
+        while(arr.indexOf(valuesToRemove[i]) >= 0){
+            let indexToRemove = arr.indexOf(valuesToRemove[i]);
+            arr.splice(indexToRemove, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line

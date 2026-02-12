8c12c8ba6c095e2fcd8c3f72fd9f414d06ccfe0d
Pontificust <lihtinmann88@yandex.ru>
2026-02-12 16:58:45 +0300

    fix: use parseInt to handle with string input

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
 const fibonacci = function(n) {
-    if(n < 0 || !isFinite(n) || typeof n !== 'number' && !n){
+    if(n < 0){
         return 'OOPS';
+    } else {
+        n = typeof n === 'number' ? n : parseInt(n);
     }
     const goldenRatio = 0.5 * (1 + Math.sqrt(5));
     const goldenRationConjugate = 0.5 * (1 - Math.sqrt(5));

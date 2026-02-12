596f344dc703e20c0a8fc2137fad4bba272083dd
Pontificust <lihtinmann88@yandex.ru>
2026-02-12 17:22:15 +0300

    feat: solve 16_findTheOldest task

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((a, b) => {
+        const aBirth = a.yearOfBirth;
+        const bBirth = b.yearOfBirth;
+        const aDeath = a.yearOfDeath || null;
+        const bDeath = b.yearOfDeath || null;
+        let currentYear = new Date().getFullYear();
+        if(aDeath && bDeath) {
+            return aDeath - aBirth - bDeath + bBirth;
+        } else if(!aDeath && bDeath) {
+            return currentYear - aBirth - bDeath + bBirth;
+        } else if(aDeath && !bDeath) {
+            return aDeath - aBirth - currentYear + bBirth;
+        } else {
+            return bBirth - aBirth;
+        }
+    }).at(-1);
 };
 
 // Do not edit below this line

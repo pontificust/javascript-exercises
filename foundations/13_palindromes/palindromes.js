d1ee18ed7b5b4051b127646b67da8123d99697e7
Pontificust <lihtinmann88@yandex.ru>
2026-02-12 16:26:02 +0300

    feat: implement palingrome function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const lowerCaseStr = str.toLowerCase();
+    const strWithoutPunctuation = 
+    lowerCaseStr.split('').filter(val => 
+        val.codePointAt(0) >= 'a'.codePointAt(0) && 
+        val.codePointAt(0) <= 'z'.codePointAt(0) ||
+        val.codePointAt(0) >= '0'.codePointAt(0) &&
+        val.codePointAt(0) <= '9'.codePointAt(0)).join('');
+    console.log(strWithoutPunctuation);
+    return (
+        strWithoutPunctuation.split('').reverse().join('') === strWithoutPunctuation);    
 };
 
 // Do not edit below this line

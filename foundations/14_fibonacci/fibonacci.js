dfe47859e1c9006725d03549fb3814a919308cdb
Pontificust <lihtinmann88@yandex.ru>
2026-02-12 16:50:42 +0300

    feat: implement fibonacci function

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0 || !isFinite(n) || typeof n !== 'number' && !n){
+        return 'OOPS';
+    }
+    const goldenRatio = 0.5 * (1 + Math.sqrt(5));
+    const goldenRationConjugate = 0.5 * (1 - Math.sqrt(5));
 
+    const nthFibonacci = (goldenRatio ** n - goldenRationConjugate ** n) / Math.sqrt(5);
+    return Math.round(nthFibonacci);
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;

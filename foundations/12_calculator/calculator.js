830969514c6e7a5b0b504af6587f24f93a1b5425
Pontificust <lihtinmann88@yandex.ru>
2026-02-12 14:33:25 +0300

    feat: solve the 12_calculator task

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(addend1, addend2) {
+	return addend1 + addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc, number) => acc + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((acc, number) => acc * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+	for(let i = 2; i <= number; i += 1) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
